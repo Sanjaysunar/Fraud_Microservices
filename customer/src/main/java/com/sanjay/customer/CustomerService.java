bef9901057b2c757ea176c104fec4039653a05a1
Sanjay Sunar <sansunar11@gmail.com>
2023-07-26 23:54:53 +0530

    Connecting to DB

--- customer/src/main/java/com/sanjay/customer/CustomerService.java
+++ customer/src/main/java/com/sanjay/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.sanjay.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
 
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder()
@@ -15,5 +15,6 @@ public record CustomerService() {
         // todo : check if email valid
         // todo : check if email not taken
         // todo : store customer in db
+        customerRepository.save(customer);
     }
 }
