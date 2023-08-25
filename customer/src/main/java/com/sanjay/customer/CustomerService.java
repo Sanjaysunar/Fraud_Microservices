24476bf0d44d032f3a2e1a789715b592b6b50648
Sanjay Sunar <sansunar11@gmail.com>
2023-08-25 15:50:51 +0530

    restTemplate error solve

--- customer/src/main/java/com/sanjay/customer/CustomerService.java
+++ customer/src/main/java/com/sanjay/customer/CustomerService.java
@@ -1,6 +1,7 @@
 package com.sanjay.customer;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -21,7 +22,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
         // todo: check if fraudster
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http:localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
